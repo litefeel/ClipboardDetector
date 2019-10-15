adb08c2ac0e3baa4736c20aa2edd4037fec06df2
litefeel <litefeel@gmail.com>
2019-10-15 22:57:57 +0800

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WK.Libraries.SharpClipboardNS;
+
+namespace ClipboardDetector
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SharpClipboard clipboard;
+
+        public MainWindow()
+        {
+            clipboard = new SharpClipboard();
+
+            clipboard.ObservableFormats.Texts = true;
+            clipboard.ObservableFormats.Files = true;
+            clipboard.ObservableFormats.Images = true;
+            clipboard.ObservableFormats.Others = true;
+            clipboard.ClipboardChanged += ClipboardChanged;
+
+            Console.WriteLine("==================");
+
+            InitializeComponent();
+
+            Console.WriteLine("==================");
+            myTxt.Text = "++++++++++++++";
+        }
+
+        private void ClipboardChanged(object sender, SharpClipboard.ClipboardChangedEventArgs e)
+        {
+            myTxt.Text = $"ClipboardChanged {e.ContentType}";
+
+            Console.WriteLine($"ClipboardChanged {e.ContentType}");
+            // Is the content copied of text type?
+            if (e.ContentType == SharpClipboard.ContentTypes.Text)
+            {
+                // Get the cut/copied text.
+                Console.WriteLine(clipboard.ClipboardText);
+                myTxt.Text = $"ClipboardChanged {e.ContentType} {clipboard.ClipboardText}";
+                myTxt.Width = myTxt.ActualWidth;
+            }
+
+            // Is the content copied of image type?
+            else if (e.ContentType == SharpClipboard.ContentTypes.Image)
+            {
+                
+                // Get the cut/copied image.
+                //var img = clipboard.ClipboardImage;
+            }
+
+            // Is the content copied of file type?
+            else if (e.ContentType == SharpClipboard.ContentTypes.Files)
+            {
+                // Get the cut/copied file/files.
+                //Debug.WriteLine(clipboard.ClipboardFiles.ToArray());
+
+                // ...or use 'ClipboardFile' to get a single copied file.
+                //Debug.WriteLine(clipboard.ClipboardFile);
+            }
+
+            // If the cut/copied content is complex, use 'Other'.
+            else if (e.ContentType == SharpClipboard.ContentTypes.Other)
+            {
+                // Do something with 'clipboard.ClipboardObject' or 'e.Content' here...
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
